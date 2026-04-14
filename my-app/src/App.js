dd7e1d3794b91a0d54c373e8fa488654427ee017
muzammilali5994 <muzammilali5994@outlook.com>
2026-04-14 13:53:43 +0500

    react

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <div className="min-h-screen flex items-center justify-center bg-blue-500 text-white text-4xl">
+      Tailwind is Working ✅
+    </div>
+  );
+}
+
+export default App;
