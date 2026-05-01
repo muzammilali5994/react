59cb8c1883a13d5d9b26012ae2c697b5824ca0e3
muzammilali5994 <muzammilali5994@outlook.com>
2026-05-01 23:12:32 +0500

    update

---
+++ react-routing/src/App.jsx
@@ -0,0 +1,14 @@
+import {BrowserRouter,Router,Route} from 'react-router-dom'
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Router>
+      <Route />
+    </Router>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ react-routing/src/pages/html.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function html() {
+  return (
+    <div>html</div>
+  )
+}
+
+export default html
